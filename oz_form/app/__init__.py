117843122d944e470da12cf2b8cf35bc826c6e45
HyunJu Lee <lhj7826@naver.com>
2025-07-21 15:13:52 +0900

    fix: init 스페이스 제거

--- oz_form/app/__init__.py
+++ oz_form/app/__init__.py
@@ -14,18 +14,16 @@ def create_app():
     application.secret_key = "oz_form_secret"
 
     db.init_app(application)
-
     migrate.init_app(application, db)
 
-		# 400 에러 발생 시, JSON 형태로 응답 반환
+    # 400 에러 발생 시, JSON 형태로 응답 반환
     @application.errorhandler(400)
     def handle_bad_request(error):
         response = jsonify({"message": error.description})
         response.status_code = 400
         return response
 
-		# app/route/__init__.py에 블루 브린트를 등록해주세요
-	  register_routes(application)
-
+    # app/routes/__init__.py에 블루프린트를 등록
+    register_routes(application)
 
     return application
