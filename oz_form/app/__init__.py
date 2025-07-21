f60ae1b9c529ed465aef019fd36ea1e8310c8814
ing2720 <dlguddns2@naver.com>
2025-07-21 15:50:56 +0900

    fix: __init__.py 파일 수정

--- oz_form/app/__init__.py
+++ oz_form/app/__init__.py
@@ -8,24 +8,19 @@ migrate = Migrate()
 
 
 def create_app():
-    application = Flask(__name__)
+	application = Flask(__name__)
+	application.config.from_object("config.Config")
+	application.secret_key = "oz_form_secret"
 
-    application.config.from_object("config.Config")
-    application.secret_key = "oz_form_secret"
+	db.init_app(application)
+	migrate.init_app(application, db)
 
-    db.init_app(application)
+	@application.errorhandler(400)
+	def handle_bad_request(error):
+		response = jsonify({"message": error.description})
+		response.status_code = 400
+		return response
 
-    migrate.init_app(application, db)
+	register_routes(application)
 
-		# 400 에러 발생 시, JSON 형태로 응답 반환
-    @application.errorhandler(400)
-    def handle_bad_request(error):
-        response = jsonify({"message": error.description})
-        response.status_code = 400
-        return response
-
-		# app/route/__init__.py에 블루 브린트를 등록해주세요
-	  register_routes(application)
-
-
-    return application
+	return application
